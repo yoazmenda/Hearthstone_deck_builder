0ed168f373442153788f54415a48ec66199b5ea4
yoazmenda <yoazmenda@gmail.com>
2015-06-17 13:13:56 +0300

    fixed performence issue regarding random deck creation

--- main.py
+++ main.py
@@ -6,59 +6,59 @@ import timeit
 import random
 
 
-card_names = []
-
+cards = []
 
 def create_random_deck():
     neutrals = random.choice(range(0,31))
-    cards = []
+    character_class = random.choice(range(1,10)) #only normal heroes and must be one of them
+    deck = [] #result
     count = 0
-    #select neutrals:
+    naturals_cards = list(filter(lambda card: card.character_class == CHARACTER_CLASS.ALL, cards)) #filter out hero cards
+    class_cards = list(filter(lambda card: card.character_class == character_class, cards)) 
+    print(naturals_cards)
+    print("--------------------------------------------------")
+    print(class_cards)
     while count < neutrals:
-        card = card_lookup(random.choice(card_names))
-        if card.character_class != CHARACTER_CLASS.ALL:
-            continue
+        card = random.choice(naturals_cards)
         prev_count = 0
-        for prev in cards:       
+        for prev in deck:  #count previous occurrences
             if prev.name == card.name:
                 prev_count += 1
         if prev_count == 2:
-            continue          
-        cards.append(card)
+            naturals_cards.remove(card)
+            continue                
+        deck.append(card)
         count += 1
-    
-    
-    #select class specific:
-    character_class = random.choice(range(1,10))
+
     while count < 30:
-        card = card_lookup(random.choice(card_names))
-        if card.character_class != character_class:
-            continue
+        card = random.choice(class_cards)
         prev_count = 0
-        for prev in cards:       
+        for prev in cards:  #count previous occurrences
             if prev.name == card.name:
                 prev_count += 1
         if prev_count == 2:
-            continue          
-        cards.append(card)
+            class_cards.remove(card)
+            continue                
+        deck.append(card)
         count += 1
-    return Deck(cards, hero_for_class(character_class))
+    print(len(deck))
+    return Deck(deck, hero_for_class(character_class))
      
 
      
 def init_system():
-    create_all_card_names()
+    create_cards()
 
-def create_all_card_names():
+def create_cards():
     with open("card_defs.json", "r") as file:
-        all_cards = json.load(file)
-        for card in all_cards:
-            card_names.append(card['name']) 
+        json_cards = json.load(file)
+        for card in json_cards:
+            cards.append(card_lookup(card['name']))
   
 def init_population(pop_size):  
     print("initializing population")
     decks = []
-    for _ in range(0,pop_size):
+    for _i in range(0,pop_size):
         decks.append(create_random_deck())  
     print("finish initializing population")
     return decks
@@ -78,24 +78,21 @@ def mutate(population):
 def start():
     init_system()  
     pop_size = 8 # population size
-    generation_limit = 20 # stopping condition - adter 20 generations
+    generation_limit = 20 # stopping condition
     generation = 0  
-    population = init_population(pop_size) #list of N randomized individuals (decks) with fittness = 0    
+    population = init_population(pop_size) #list of N randomized individuals (decks) with fitness = 0
+    
     #Genetic Algorithm
     while generation < generation_limit: #stopping condition
-        population = evaluate(population) #make a single-elimination-tournament and assign fittness to n individuals
-        mating_pool = select_parents(population) #use fittness proportionate selection (roulette wheel technique) to select parents
+        population = evaluate(population) #make a single-elimination-tournament and assign fitness to n individuals
+        mating_pool = select_parents(population) #use fitness proportioned selection (roulette wheel technique) to select parents
         population = do_crossover(mating_pool) #create next generation from mating pool with crossover. survivor selection: children replace parents
         population = mutate(population) #choose some individuals and mutate them
         generation += 1
   
-  
-    #return some individual from final population
-    #winner = population[0]
-    print("-------------------------------------------------------")
-    print ("Winner is: %s" %"winner")
-    print("-------------------------------------------------------")
+
 
 
 if __name__ == "__main__":
     print(timeit.timeit(start, 'gc.enable()', number=1))
+    print("Done")
